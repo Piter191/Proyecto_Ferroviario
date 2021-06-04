155c290ebed96bd4cc120845a3ee2234b055f682
IIVixoII <naruto.sennin090@gmail.com>
2021-06-04 14:52:46 -0400

    Intento Mapa 3.0

--- Dashboard_Admin.php
+++ Dashboard_Admin.php
@@ -74,6 +74,7 @@
   <script src='js/dashboard_admin_usuario.js'></script>
   <script src='js/dashboard_admin_trabajador.js'></script>
   <script src='js/dashboard_admin_tren.js'></script>
+  <script src='js/dashboard_mapa.js'></script>
   <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
