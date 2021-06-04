418e730ef81f19c93755f94f28f51c40723425e0
IIVixoII <naruto.sennin090@gmail.com>
2021-06-04 14:35:56 -0400

    Reubicacion de archivos

--- Dashboard_Admin.php
+++ Dashboard_Admin.php
@@ -71,9 +71,9 @@
     integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
     crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src='./dashboard_admin_usuario.js'></script>
-  <script src='./dashboard_admin_trabajador.js'></script>
-  <script src='./dashboard_admin_tren.js'></script>
+  <script src='js/dashboard_admin_usuario.js'></script>
+  <script src='js/dashboard_admin_trabajador.js'></script>
+  <script src='js/dashboard_admin_tren.js'></script>
   <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
