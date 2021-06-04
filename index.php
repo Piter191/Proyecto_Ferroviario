4a6b2db542c7d6dcf7cf6aa6c0f585bca48e6e6b
Yulian <ballena_enojada@hotmail.com>
2021-06-04 16:06:44 -0400

    Pequeño Cambio pantalla de inicio

--- index.php
+++ index.php
@@ -85,8 +85,6 @@ https://templatemo.com/tm-536-dream-pulse
                 </li>
               </ul>
               <footer class="text-center text-white small">
-                <p class="mb--0 mb-2">Copyright 2020 Dream Pulse</p>
-                <p class="mb-0">Design:
                   <a rel="nofollow" href="https://templatemo.com" class="tm-footer-link">Template Mo</a>
                 </p>
               </footer>
