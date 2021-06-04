f77ca7d2758c29f4499f7b97ed5816bd27ab2063
IIVixoII <naruto.sennin090@gmail.com>
2021-06-04 16:58:31 -0400

    Mapa Mina Shapes

---
+++ mapa_mina.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mapa</title>
+</head>
+<body>
+<img src="img/Mapa Mina.png" alt="" width="918" height="739" usemap="#Mapa_MinaMap" id="Mapa_Mina" title="Mapa_Mina"/>
+<map name="Mapa_MinaMap">
+  <area shape="poly" coords="250,671" href="#">
+  <area shape="poly" coords="250,671,227,531,286,454,341,507" href="#" alt="Puente de Vaciado">
+  <area shape="poly" coords="154,667,126,510,203,431,195,567" href="#" alt="Talleres">
+  <area shape="rect" coords="514,250,792,330" href="#" alt="Interior Mina Granito">
+</map>
+	
+</body>
+</html>
