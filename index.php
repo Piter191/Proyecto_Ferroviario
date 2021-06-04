8e82e0280753984961b41b9afb853390e24e9abe
IIVixoII <naruto.sennin090@gmail.com>
2021-06-04 16:29:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -125,19 +125,19 @@ https://templatemo.com/tm-536-dream-pulse
                   <div class="tm-contact-form-wrap">
                     <form action="" method="POST" class="tm-contact-form">
                         <div class="form-group">
-                          <input type="text" id="contact_name" name="contact_name" class="form-control rounded-0 border-top-0 border-right-0 border-left-0" placeholder="Your Name" required="" />
+                          <input type="text" id="contact_name" name="contact_name" class="form-control rounded-0 border-top-0 border-right-0 border-left-0" placeholder="Tu Nombre" required="" />
                         </div>
                         <div class="form-group">
-                          <input type="email" id="contact_email" name="contact_email" class="form-control rounded-0 border-top-0 border-right-0 border-left-0" placeholder="Your Email" required="" />
+                          <input type="email" id="contact_email" name="contact_email" class="form-control rounded-0 border-top-0 border-right-0 border-left-0" placeholder="Tu Email" required="" />
                         </div>
                 
                         <div class="form-group">
-                          <textarea rows="4" id="contact_message" name="contact_message" class="form-control rounded-0 border-top-0 border-right-0 border-left-0" placeholder="Message..." required=""></textarea>
+                          <textarea rows="4" id="contact_message" name="contact_message" class="form-control rounded-0 border-top-0 border-right-0 border-left-0" placeholder="Mensaje" required=""></textarea>
                         </div>
                 
                         <div class="form-group mb-0">
                           <button type="submit" class="btn rounded-0 d-block ml-auto tm-btn-primary">
-                            SEND
+                            Enviar
                           </button>
                         </div>
                       </form>
