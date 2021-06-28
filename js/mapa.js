8e0175d661e29f0893f1fb84f82e354daf48a59b
IIVixoII <naruto.sennin090@gmail.com>
2021-06-28 18:39:16 -0400

    Termino Areas Mapa (Shapes)

--- js/mapa.js
+++ js/mapa.js
@@ -6,10 +6,12 @@ $('#mapaId').on('click', () => {
 
     <img src="img/Mapa Mina.png" alt="" width="918" height="739" usemap="#Mapa_MinaMap" id="Mapa_Mina" title="Mapa_Mina"/>
     <map name="Mapa_MinaMap">
-      <area shape="poly" coords="250,671" href="#">
-      <area shape="poly" coords="250,671,227,531,286,454,341,507" href="#" alt="Puente de Vaciado">
-      <area shape="poly" coords="154,667,126,510,203,431,195,567" href="#" alt="Talleres">
-      <area shape="rect" coords="514,250,792,330" href="#" alt="Interior Mina Granito">
+  <area shape="poly" coords="250,671" href="#">
+  <area shape="poly" coords="250,671,227,531,301,365,341,507" href="#" alt="Puente de Vaciado">
+  <area shape="poly" coords="154,667,115,482,223,386,195,567" href="#" alt="Talleres">
+  <area shape="rect" coords="514,250,792,330" href="#" alt="Interior Mina Granito">
+  <area shape="rect" coords="444,22,857,240" href="#" alt="3 Lineas">
+  <area shape="poly" coords="287,316,347,371,471,245" href="#" alt="Portal Tunel">
     </map>
         <center>
     `;
