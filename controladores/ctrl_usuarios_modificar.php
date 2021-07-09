e0e2507d273738f0e8550926beaa88b9920920d9
Eduardo <ejaquek@gmail.com>
2021-07-09 01:57:57 -0400

    arregle el update de estado en usuario

--- controladores/ctrl_usuarios_modificar.php
+++ controladores/ctrl_usuarios_modificar.php
@@ -9,13 +9,14 @@ $nombre=$_POST["nombre"];
 $rut=$_POST["rut"];
 $tipo_usuario=$_POST["tipo_usuario"];
 $clave=$_POST["clave"];
+$estado=$_POST["estado"];
 $id_usuario=$_POST["id_usuario"];
 
 
 
 $sql = "UPDATE usuario SET nombre = '$nombre', rut = '$rut', tipo_usuario = '$tipo_usuario', clave = '$clave', estado = '$estado' where id_usuario=".$id_usuario;
 	mysqli_query(conectar(),$sql);
-	header('Location:../Dashboard_Admin.php');
+	//header('Location:../Dashboard_Admin.php');
 
-
+echo $sql;
 ?>
