eafc2021eadc780c2681d86b5c7795d3258b6e81
Spider-Diull <diego.ulloa1360@gmail.com>
2021-07-11 23:23:08 -0500

    Probando la validacion del rut por medio de echo

--- controladores/ctrl_usuarios_ingresar.php
+++ controladores/ctrl_usuarios_ingresar.php
@@ -2,13 +2,16 @@
 include('../funciones/setup.php');	
 if ( valida_rut($_POST['rut']) )
 {
-	$sql = "INSERT INTO usuario (nombre,rut,tipo_usuario,clave,estado) VALUES ('".$_POST['nombre']."','".$_POST['rut']."','".$_POST['tipo_usuario']."','".$_POST['clave']."','".$_POST['estado']."')";
+	/*$sql = "INSERT INTO usuario (nombre,rut,tipo_usuario,clave,estado) VALUES ('".$_POST['nombre']."','".$_POST['rut']."','".$_POST['tipo_usuario']."','".$_POST['clave']."','".$_POST['estado']."')";
 	mysqli_query(conectar(),$sql);
-	header('Location:../Dashboard_Admin.php');
+	
+	header('Location:../Dashboard_Admin.php');*/
+	echo 'el rut es CORRECTO :-)';
 }
 else
 {
-	header('Location:../index.php');
+	/*header('Location:../index.php');*/
+	echo 'el rut es incorrecto :-(';
 }
 
 ?>
