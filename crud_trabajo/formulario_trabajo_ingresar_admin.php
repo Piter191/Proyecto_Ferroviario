8168ca3350f1124801434e1f8b03a341b9a3fbe3
Vicente Alejandro Morales Araya <naruto.sennin090@gmail.com>
2021-07-12 00:35:56 -0400

    Test Modal para el mapa en formulario de Trabajo y cambio al nombre del archivo png del mapa

--- crud_trabajo/formulario_trabajo_ingresar_admin.php
+++ crud_trabajo/formulario_trabajo_ingresar_admin.php
@@ -2,9 +2,11 @@
 <html lang="en">
 
 <head>
-	<title>Formulario Usuario</title>
+	<link rel="icon" href="img/favicon_geotren.ico" type="image/x-icon" />
+	<title>Ingreso de Trabajo</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
 	<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/train.ico" />
 	<!--===============================================================================================-->
@@ -27,9 +29,7 @@
 	<link rel="stylesheet" type="text/css" href="../css/util.css">
 	<link rel="stylesheet" type="text/css" href="../css/iniciar_sesion/main.css">
 	<!--===============================================================================================-->
-	<link rel="icon" href="img/favicon_geotren.ico" type="image/x-icon" />
 
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
 
 </head>
@@ -58,7 +58,7 @@
 			</p>
 			<p>
 				<center> <label for="hora_termino">Hora de Termino</label>
-						<input type="time" name="hora_termino" id="hora_termino" required>
+					<input type="time" name="hora_termino" id="hora_termino" required>
 				</center>
 			</p>
 			<p>
@@ -81,35 +81,63 @@
 		</div>
 		</p>
 		<center> <label for="password">Estado</label> </center>
-		<div class="select" >
-		<center>
-		<select name="estado" id="estado" style="width:250px">
-				<option disabled="disabled" selected="selected">Seleccionar</option>
-				<option>Iniciado</option>
-				<option>En Progreso</option>
-				<option>Terminado</option>
-				<option>Suspendido</option>
-				<option>Interrumpido</option>
-			</select>
-		</center>
+		<div class="select">
+			<center>
+				<select name="estado" id="estado" style="width:250px">
+					<option disabled="disabled" selected="selected">Seleccionar</option>
+					<option>Iniciado</option>
+					<option>En Progreso</option>
+					<option>Terminado</option>
+					<option>Suspendido</option>
+					<option>Interrumpido</option>
+				</select>
+			</center>
 		</div>
 		</p> <br>
-		<br>
 		<center>
-			<input type="submit" name="go" id="go" value="Registrar">
-			<br> <br>
+			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+				Ver Mapa
+			</button>
 
-		</center>
+			<!--Bootstrap modal -->
+			<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<!-- Modal heading -->
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">
+								Mapa Mina
+							</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">
+									×
+								</span>
+							</button>
+						</div>
+						<!-- Modal body with image -->
+						<div class="modal-body">
+							<img src="/img/Mapa_Mina.png">
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<center>
+				<input type="submit" name="go" id="go" value="Registrar">
+				<br> <br>
+
+			</center>
 
 	</form>
 
 </body>
 
 </html>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src='formulario_trabajo.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
 <script type="text/javascript">
 	$(document).ready(function() {
