3095ccecf28e20d9b2850b8ced0c05f155ec742c
Spider-Diull <diego.ulloa1360@gmail.com>
2021-07-11 23:45:14 -0500

    El controlador usuarios ingresar lo delvolvi a su estado anterior

--- controladores/ctrl_usuarios_ingresar.php
+++ controladores/ctrl_usuarios_ingresar.php
@@ -1,17 +1,7 @@
 <?php
 include('../funciones/setup.php');	
-if ( valida_rut($_POST['rut']) )
-{
-	/*$sql = "INSERT INTO usuario (nombre,rut,tipo_usuario,clave,estado) VALUES ('".$_POST['nombre']."','".$_POST['rut']."','".$_POST['tipo_usuario']."','".$_POST['clave']."','".$_POST['estado']."')";
-	mysqli_query(conectar(),$sql);
-	
-	header('Location:../Dashboard_Admin.php');*/
-	echo 'el rut es CORRECTO :-)';
-}
-else
-{
-	/*header('Location:../index.php');*/
-	echo 'el rut es incorrecto :-(';
-}
 
+$sql = "INSERT INTO usuario (nombre,rut,tipo_usuario,clave,estado) VALUES ('".$_POST['nombre']."','".$_POST['rut']."','".$_POST['tipo_usuario']."','".$_POST['clave']."','".$_POST['estado']."')";
+	mysqli_query(conectar(),$sql);
+	header('Location:../Dashboard_Admin.php');
 ?>
