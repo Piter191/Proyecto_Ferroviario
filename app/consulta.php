fbf423960448726c09b2b751079b72278163e988
Spider-Diull <diego.ulloa1360@gmail.com>
2021-07-16 21:02:02 -0500

    Cambio en la consulta.php

--- app/consulta.php
+++ app/consulta.php
@@ -1,8 +1,8 @@
 <?php
 include('../funciones/setup.php');
-
+$con=mysqli_connect("localhost","root","Geotren","geotren");
 $sql="SELECT * FROM trabajo_transporte";
-$result=mysqli_query(conectar(),$sql);
+$result=mysqli_query($con,$sql);
 $arr = array();
 while ($datos=mysqli_fetch_array($result)){
     $arr[]= $datos;
