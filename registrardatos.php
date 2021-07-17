a97272dac376bc62e6cf1317268ffbf33217f5ae
Eduardo <ejaquek@gmail.com>
2021-07-17 01:08:46 -0400

    prueba de direccion desde el mapa

---
+++ registrardatos.php
@@ -0,0 +1,15 @@
+<?php
+
+$cont=mysqli_connect('localhost','root','Geotren','Geotren');
+
+$sql="select * from usuario where rut='".$_GET['usu']."' and clave='".$_GET['pass']."' and estado = 'Activo' and tipo_usuario = 'Administrador'";
+
+$result=mysqli_query($cont,$sql);
+
+$num=mysqli_num_rows($result);
+
+$val=array('estado'=>$num==null?'0':$num);
+
+echo json_encode($val);
+
+?>
