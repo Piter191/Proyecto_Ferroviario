849d8e6d2102ba90261cbc9f52b4e68309ea23f1
Vicente Alejandro Morales Araya <naruto.sennin090@gmail.com>
2021-07-17 21:35:09 -0400

    Tabla Trabajo Zona Talleres

--- Trabajos_Talleres.php
+++ Trabajos_Talleres.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Latest compiled and minified CSS -->
+	<title>Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -12,26 +13,39 @@
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-<table class="table table-striped">
-  	
+	<table class="table table-striped">
+
 		<thead>
-		<tr>
-			<th>ID</th>
-			<th>NOMBRE</th>
-			<th>APELLIDO</th>
-			
-		</tr>
+			<tr>
+				<th class="th-sm table-danger">Fecha de inicio</th>
+				<th class="th-sm table-danger">Fecha de termino</th>
+				<th class="th-sm table-danger">Trabajador</th>
+				<th class="th-sm table-danger">Lugar</th>
+				<th class="th-sm table-danger">Sector</th>
+			</tr>
 		</thead>
-<?php foreach ($link->query('SELECT * from registros') as $row){ // aca puedes hacer la consulta e iterarla con each. ?> 
-<tr>
-	<td><?php echo $row['id'] // aca te faltaba poner los echo para que se muestre el valor de la variable.  ?></td>
-    <td><?php echo $row['nombre'] ?></td>
-    <td><?php echo $row['apellido'] ?></td>
- </tr>
-<?php
-	}
-?>
-</table>
+		<?php
+
+		$sql = "SELECT comunas.nombre_comunas, usuarios.* FROM comunas INNER JOIN usuarios ON usuarios.id_comunas = comunas.id_comunas";
+		$result = mysqli_query(conectar(), $sql); // EJECUTAMOS LA QUERY
+		while ($dato = mysqli_fetch_array($result)) {
+
+		?>
+			?>
+			<tr>
+				<td><?php echo $dato['Fecha_Inicio'] // aca te faltaba poner los echo para que se muestre el valor de la variable.
+					?></td>
+				<td><?php echo $dato['Fecha_Termino'] ?></td>
+				<td><?php echo $dato['Trabajador'] ?></td>
+				<td><?php echo $dato['Lugar'] ?></td>
+				<td><?php echo $dato['Sector'] ?></td>
+			</tr>
+		<?php
+		}
+		?>
+	</table>
 </body>
+
 </html>
--- js/mapa.js
+++ js/mapa.js
@@ -1,5 +1,5 @@
 $('#mapaId').on('click', () => {
-    const text=`
+  const text = `
     <center>
     <h1>Mapa de la mina</h1>
 
@@ -8,15 +8,15 @@ $('#mapaId').on('click', () => {
     <map name="Mapa_MinaMap">
   <area shape="poly" coords="250,671" href="#">
   <area shape="poly" coords="250,671,227,531,301,365,341,507" href="./index.php" alt="Puente de Vaciado">
-  <area shape="poly" coords="154,667,115,482,223,386,195,567" href="#" alt="Talleres">
+  <area shape="poly" coords="154,667,115,482,223,386,195,567" href="./Trabajos_Talleres.php" alt="Talleres">
   <area shape="rect" coords="514,250,792,330" href="#" alt="Interior Mina Granito">
   <area shape="rect" coords="444,22,857,240" href="#" alt="3 Lineas">
   <area shape="poly" coords="287,316,347,371,471,245" href="#" alt="Portal Tunel">
     </map>
         <center>
     `;
-    $('#table').empty();
-    $('#inicio').empty();
-    $('#mapa').html(text);
-    
-    })
+  $('#table').empty();
+  $('#inicio').empty();
+  $('#mapa').html(text);
+
+})
